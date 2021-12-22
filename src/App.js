6a9cef661f784f87573557c70d87deb58eb515fb
dachscafe <dachscafe@yahoo.co.jp>
2021-12-22 20:49:54 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React!
+//         </a>
+//       </header>
+//     </div>
+    
+//   );
+// }
+const RegisterText = (props) =>{
+  return (
+    <p>Hi, {props.registerUserName}!</p>
+  )
+}
+
+const RegisterUserProfile = (props) =>{
+  return (
+    <div>
+      <p>{props.registerUserAge}</p>
+      <p>{props.registerUserCountry}</p>
+    </div>
+    
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <RegisterText registerUserName="Jack"></RegisterText>
+      <RegisterUserProfile registerUserAge={22} registerUserCountry="US"></RegisterUserProfile>
+      <RegisterText registerUserName="Mary"></RegisterText>
+      <RegisterUserProfile registerUserAge={20} registerUserCountry="UK"></RegisterUserProfile>
+    </div>
+  )
+}
+
+export default App;
